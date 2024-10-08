292a96cfb83d51bf8d451e08312bcb60f640f411
Cristofor4ik <xtinabruscaginxxx@gmail.com>
2024-10-08 03:02:24 +0300

    Initial commit

---
+++ sum.test.js
@@ -0,0 +1,16 @@
+const sum = require('./sum'); 
+
+// Test1
+test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+});
+
+// Test2
+test('adds -1 + 1 to equal 0', () => {
+    expect(sum(-1, 1)).toBe(0);
+});
+
+// Test3
+test('adds 0 + 0 to equal 0', () => {
+    expect(sum(0, 0)).toBe(0);
+});
